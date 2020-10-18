39ada448a1173f6983ed577af9dbbc8004e1725f
PanosRCng <panosrcng@gmail.com>
2020-10-19 01:50:42 +0300

    init commit

--- attiny85_firmware/attiny85_firmware.ino
+++ attiny85_firmware/attiny85_firmware.ino
@@ -18,10 +18,9 @@
 #define WIFI_WAKEUP_DELAY 5000
 #define SEND_DELAY 1000
 
-#define SLEEP 0
-#define SAMPLE 1
-#define SEND_MSG 2
-
+#define SAMPLE 0
+#define SEND_MSG 1
+#define SLEEP 2
 
 
 
@@ -35,6 +34,7 @@ String msg;
 int resample_number;
 int t_sample;
 int h_sample;
+int l_sample;
 
 
 
@@ -60,6 +60,25 @@ void system_sleep()
     
   // sleep_disable
   MCUCR &= ~(1 << SE);
+}
+
+
+void initADC()
+{
+  ADMUX =
+            (1 << ADLAR) |     // left shift result
+            (0 << REFS1) |     // Sets ref. voltage to VCC, bit 1
+            (0 << REFS0) |     // Sets ref. voltage to VCC, bit 0
+            (0 << MUX3)  |     // use ADC1 for input (PB2), MUX bit 3
+            (0 << MUX2)  |     // use ADC1 for input (PB2), MUX bit 2
+            (0 << MUX1)  |     // use ADC1 for input (PB2), MUX bit 1
+            (1 << MUX0);       // use ADC1 for input (PB2), MUX bit 0
+
+  ADCSRA = 
+            (1 << ADEN)  |     // Enable ADC 
+            (1 << ADPS2) |     // set prescaler to 64, bit 2 
+            (1 << ADPS1) |     // set prescaler to 64, bit 1 
+            (0 << ADPS0);      // set prescaler to 64, bit 0  
 }
 
 
@@ -96,7 +115,17 @@ void setup_watchdog(int ii)
 }
 
 
-void sampling()
+void sample_light()
+{
+  ADCSRA |= (1 << ADSC);
+  
+  while(ADCSRA & (1 << ADSC));
+  
+  l_sample = ADCH;
+}
+
+
+void sample_temp_humi()
 {
   switch( sensor.read() )
   {
@@ -125,8 +154,17 @@ void sampling()
     default:
       t_sample = -1;
       h_sample = -1;
+      l_sample = -1;
       break;  
   }
+}
+
+
+void sampling()
+{
+  sample_light();
+
+  sample_temp_humi();
 }
 
 
@@ -138,6 +176,9 @@ void getMsg()
   msg += ", ";
   msg += "\"Humidity\": ";
   msg += h_sample;
+  msg += ", ";
+  msg += "\"Light\": ";
+  msg += l_sample;
   msg += "}";
   msg += "#";
 }
@@ -147,23 +188,10 @@ void move2FirstState()
 {
   t_sample = 0;
   h_sample = 0;
+  l_sample = 0;
   resample_number = 0;
   msg = "";
-  watchdog_counter = 0;
-  state = SLEEP;
-}
-
-
-void sleep()
-{
-  //~ 15 min
-  if(watchdog_counter > 90)
-  {
-    state = SAMPLE;
-    return;
-  }
-
-  system_sleep();
+  state = SAMPLE;
 }
 
 
@@ -189,10 +217,23 @@ void sendMsg()
   delay(WIFI_WAKEUP_DELAY);
   
   mySerial.print(msg);
-
   delay(SEND_DELAY);
 
-  move2FirstState();
+  watchdog_counter = 0;
+  state = SLEEP;
+}
+
+
+void sleep()
+{
+  //~ 15 min
+  if(watchdog_counter > 90)
+  {
+    move2FirstState();
+    return;
+  }
+
+  system_sleep();
 }
 
 
@@ -207,12 +248,14 @@ ISR(WDT_vect)
 
 void setup()
 {
-  mySerial.begin(SERIAL_BAUD);
+  initADC();
   
-  move2FirstState();
+  mySerial.begin(SERIAL_BAUD);
 
   // ~ 10s
   setup_watchdog(9);
+
+  move2FirstState();
 }
 
 
@@ -221,16 +264,16 @@ void loop()
 {
   switch(state)
   {
-    case SLEEP:
-      sleep();
-      break;
-
     case SAMPLE:
       sample();
       break;
 
     case SEND_MSG:
       sendMsg();
+      break;
+
+    case SLEEP:
+      sleep();
       break;
   }
 }
